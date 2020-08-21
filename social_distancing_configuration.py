84674fa1ac2b297e1a73f04247e70e750c17bca8
SHASHANK SINGH <shashank333kvsc@gmail.com>
2020-08-21 16:23:58 +0530

    Add files via upload

---
+++ social_distancing_configuration.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 19 12:35:21 2020
+
+@author: hp
+"""
+#Basic path to Yolo- Dir
+MODEL_PATH ="yolo-coco"
+
+#Min Probability of Object
+MIN_CONF = 0.3
+
+#Minimum NON_MAXIMA SUPRESSION threshlod
+NMS_THRESH = 0.3
+
+#Usage of GPU
+USE_GPU = False
+
+#Min Social-Distance
+MIN_DISTANCE = 50
